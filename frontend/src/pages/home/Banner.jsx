b8abc0460bba25378f0b072a019404bf0ad4fc30
Carbon2301 <trinhhuuan372@gmail.com>
2025-05-10 18:38:23 +0700

    Banner Section

---
+++ frontend/src/pages/home/Banner.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import bannerImg from '../../assets/banner.png'
+
+export const Banner = () => {
+  return (
+    <div className='flex flex-col md:flex-row-reverse py-16 justify-center items-center gap-12'>
+        <div className='md:w-1/2 w-full flex items-center md:justify-end'>
+            <img src={bannerImg} alt="banner" />
+        </div>
+
+        <div className='md:w-1/2 w-full'>
+            <h1 className='md:text-5xl text-2xl font-medium mb-7'>Sách mới tuần này</h1>
+            <p className='mb-10'>Đã đến lúc cập nhật danh sách đọc sách của bạn với một số tác phẩm
+                mới nhất và hay nhất trong thế giới văn học. Từ những cuốn tiểu thuyết giật gân
+                đầy kịch tính đến những hồi ký cuốn hút, loạt sách mới phát hành trong tuần này
+                có điều gì đó dành cho tất cả mọi người.
+            </p>
+
+            <button className='btn-primary'>Subcribe</button>
+        </div>
+
+    </div>
+  )
+}
+
+export default Banner;
--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 import App from '../App'
-
+import Home from '../pages/home/Home'
 const router = createBrowserRouter([
   {
     path: "/",
@@ -9,7 +9,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <h1>Home</h1>,
+        element: <Home/>,
       },
       {
         path: "/orders",
