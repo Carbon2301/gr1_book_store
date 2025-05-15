b71af49cc7d5514611963c7af1048d55681630db
Carbon2301 <trinhhuuan372@gmail.com>
2025-05-15 22:25:55 +0700

    Recommended Books

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Banner } from './Banner'
 import { TopSellers } from './TopSellers'
+import Recommended from './Recommended'
+
 export const Home = () => {
   return (
     <div>
         <Banner/>
         <TopSellers/>
+        <Recommended/>
     </div>
   )
 }
