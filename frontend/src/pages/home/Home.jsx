d975459131a0a347d40a334daed95142cbf724df
Carbon2301 <trinhhuuan372@gmail.com>
2025-05-15 23:26:08 +0700

    News Section

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { Banner } from './Banner'
 import { TopSellers } from './TopSellers'
 import Recommended from './Recommended'
-
+import News from './News'
 export const Home = () => {
   return (
     <div>
         <Banner/>
         <TopSellers/>
         <Recommended/>
+        <News/>
     </div>
   )
 }
